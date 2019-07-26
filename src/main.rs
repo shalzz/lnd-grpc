80644efd5a222031c9fdb7a1bed1d12d786db470
Shaleen Jain <shaleen@jain.sh>
2019-07-26 10:54:29 +0530

    add and use a der certificate

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use protos::rpc::{GetInfoRequest};
 use tls_api::{ TlsConnector, TlsConnectorBuilder};
 
 fn test_tls_connector() -> tls_api_native_tls::TlsConnector {
-    let root_ca = include_bytes!("../tls.cert");
+    let root_ca = include_bytes!("../tls.der");
     let root_ca = tls_api::Certificate::from_der(root_ca.to_vec());
 
     let mut builder = tls_api_native_tls::TlsConnector::builder().unwrap();
@@ -29,16 +29,17 @@ fn main() {
 
     let client_conf = Default::default();
 
-    let mut tls_option = httpbis::ClientTlsOption::Tls("127.0.0.1".to_owned(),
+    let tls_option = httpbis::ClientTlsOption::Tls("127.0.0.1".to_owned(),
                 Arc::new(test_tls_connector()));
     let addr = SocketAddr::new("::1".parse().unwrap(), port);
 
-    let grpc_client =
-            Arc::new(grpc::Client::new_expl(&addr, "127.0.0.1", tls_option, client_conf).unwrap());
+    let grpc_client = Arc::new(
+        grpc::Client::new_expl(&addr, "127.0.0.1", tls_option, client_conf).unwrap()
+    );
 
     let client = LightningClient::with_client(grpc_client);
 
-    let mut req = GetInfoRequest::new();
+    let req = GetInfoRequest::new();
 
     let resp = client.get_info(grpc::RequestOptions::new(), req);
 
